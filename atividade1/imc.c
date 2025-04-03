c96d1303545c6f8f1cc97868f97f6f531a219785
gustavobraz2006 <gustavobrazcamisa12@gmail.com>
2025-04-03 11:54:24 -0300

    classificação_imc

--- atividade1/imc.c
+++ atividade1/imc.c
@@ -12,7 +12,34 @@ int main(int argc, char* argv []){
     float alturaQuadrada = powf(altura, 2);
   
     imc = peso / alturaQuadrada;
-    printf("Seu imc é: %.2f\n", imc);
+    printf("Seu imc eh: %.2f\n", imc);
+
+    // Classificação do IMC
+
+    if(imc < 18.5) {
+        printf("Classificacao: Abaixo do peso\n");
+    } 
+    
+    if(imc > 18.5 && imc <= 24.9){
+        printf("Classificacao: Peso normal\n");
+    }
+
+    if(imc > 24.9 && imc <= 29.9){
+        printf("Classificacao: Sobrepeso\n") ;
+    }
+    
+    if (imc > 29.9 && imc <= 34.9){
+        printf("Classificacao: Obesidade I\n");
+    }
+
+    if (imc > 34.9 && imc <= 39.9){
+        printf("Classificacao: Obesidade II\n");
+    }
+
+    if (imc > 39.9 ){
+        printf("Classificacao: Obesidade III\n");
+    }
+    
 
     return 0;
 }
