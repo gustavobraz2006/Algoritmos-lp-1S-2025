e500d7c57f5ee4b5f2f77e270e7e3aa977debf27
gustavobraz2006 <gustavobrazcamisa12@gmail.com>
2025-05-23 12:29:06 -0300

    alteraçoes

--- funcao/notas5.c
+++ funcao/notas5.c
@@ -23,14 +23,15 @@ int main(int argc, char* argv[]){
 
 for (int i = 0; i < n; i++){
 
+    
     printf ("Digite o nome do aluno %d: \n", i + 1);
     getchar();
     fgets(alunos[i].nome, TAM_NOME , stdin);
     printf("Digite a nota do aluno %d: \n", i + 1);
     scanf("%f" , &alunos[i].nota);
     
-
     imprimir_aluno(alunos[i]);
+
     soma_notas += alunos[i].nota;
     if(maior_nota < alunos[i].nota){
         maior_nota = alunos[i].nota;
@@ -38,12 +39,13 @@ for (int i = 0; i < n; i++){
     if(menor_nota > alunos[i].nota){
         menor_nota = alunos[i].nota;
     }
+    
 }
-
+    
      
      nota_media = soma_notas / n;
     printf("\na nota media da turma eh %.2f\n", nota_media);
-    printf("\na maior nota da turma eh %.2f\n", maior_nota);
+    printf("\na maior nota da turma eh %.2f\n ", maior_nota);
     printf("\na menor nota da turma eh %.2f\n", menor_nota);
 
 
