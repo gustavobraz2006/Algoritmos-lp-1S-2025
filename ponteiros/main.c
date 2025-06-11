f46585ab7608f130204b7aad176bae943156b465
gustavobraz2006 <gustavobrazcamisa12@gmail.com>
2025-06-11 18:47:35 -0300

    teste

--- ponteiros/main.c
+++ ponteiros/main.c
@@ -3,35 +3,13 @@
 #include <windows.h>
 #include <string.h>
 
-#define MAX 255
-
-// Variável Global
-int y = 1;
-
-void incrementaX(int *x){
-    // Variável Local
-    while(*x < 5){
-        y++;
-        *x = 11;
-    }
-}
-
 int main(int argc, char* argv[]){
 
-    int z = 1;
-    int w = 2;
-    incrementaX (&z);
+        int x ;
 
-    printf("z = %d\n", z);
-    printf("y = %d\n", y);
-
-    printf("w = %p\n", &w);
-    printf("z = %p\n", &z);
-
-
-
-
-
+    //variáveis globais e locais 
+    // stack data program 
+    
 
     return 0;
 }    
